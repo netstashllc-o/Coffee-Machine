3687ff139f2600f4a9e6ac01b69905d9e19e6532
Meg Prescott <mprescott@ccsnh.edu>
2022-09-22 15:35:22 -0400

    Stage 2 complete

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -1,8 +1,19 @@
 const input = require('sync-input')
-console.log(`Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!`);
+const ingredients = new Map(
+    [
+        ['water', {amount: 200, unit: 'ml'}],
+        ['milk', {amount: 50, unit: 'ml'}],
+        ['coffee beans', {amount: 15, unit: 'g'}]
+    ]
+);
+
+let totals = "";
+let cups = Number(input("Write how many cups of coffee you will need:\n"));
+
+for (let item of Array.from(ingredients.keys())) {
+    let details = ingredients.get(item);
+    let totalAmount = cups * details.amount;
+    totals = totals.concat(`\n${totalAmount} ${details.unit} of ${item}`);
+}
+
+console.log(`For ${cups} cups of coffee you will need:${totals}`);
