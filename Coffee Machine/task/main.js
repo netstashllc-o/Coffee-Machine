523a09fc2fbd938ef742510dea519138e633f635
Meg Prescott <mprescott@ccsnh.edu>
2022-09-23 09:56:14 -0400

    Stage 3 complete

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -1,4 +1,4 @@
-const input = require('sync-input')
+const input = require('sync-input');
 const ingredients = new Map(
     [
         ['water', {amount: 200, unit: 'ml'}],
@@ -7,13 +7,24 @@ const ingredients = new Map(
     ]
 );
 
-let totals = "";
-let cups = Number(input("Write how many cups of coffee you will need:\n"));
+let waterAdded = Number(input("Write how many ml of water the coffee machine has:\n"));
+let milkAdded = Number(input("Write how many ml of milk the coffee machine has:\n"));
+let beansAdded = Number(input("Write how many grams of coffee beans the coffee machine has:\n"));
+let cupsNeeded = Number(input("Write how many cups of coffee you will need:\n"));
 
-for (let item of Array.from(ingredients.keys())) {
-    let details = ingredients.get(item);
-    let totalAmount = cups * details.amount;
-    totals = totals.concat(`\n${totalAmount} ${details.unit} of ${item}`);
+let cupsPossible = Math.min(
+    Math.floor(waterAdded / ingredients.get('water').amount),
+    Math.floor(milkAdded / ingredients.get('milk').amount),
+    Math.floor(beansAdded / ingredients.get('coffee beans').amount)
+);
+
+if (cupsNeeded <= cupsPossible) {
+    let result = "Yes, I can make that amount of coffee";
+    let additionalCups = cupsPossible - cupsNeeded;
+    if (additionalCups > 0) {
+        result += ` (and even ${additionalCups} more than that)`;
+    }
+    console.log(result);
+} else {
+    console.log(`No, I can make only ${cupsPossible} cups of coffee`);
 }
-
-console.log(`For ${cups} cups of coffee you will need:${totals}`);
